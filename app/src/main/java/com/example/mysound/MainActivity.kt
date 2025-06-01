031aa03d7f5a03c8ca5274e423f89e399ac35a6c
zakymzn <mamurzakynurrokhman@gmail.com>
2025-06-01 15:28:39 +0700

    result

--- app/src/main/java/com/example/mysound/MainActivity.kt
+++ app/src/main/java/com/example/mysound/MainActivity.kt
@@ -1,20 +1,46 @@
 package com.example.mysound
 
+import android.media.SoundPool
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var sp: SoundPool
+    private var soundId: Int = 0
+    private var spLoaded = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnSound = findViewById<Button>(R.id.btn_sound_pool)
+
+        sp = SoundPool.Builder()
+            .setMaxStreams(10)
+            .build()
+
+        // Tambahkan listener ke soundpool jika proses load sudah selesai
+        sp.setOnLoadCompleteListener { _, _, status ->
+            if (status == 0) {
+                spLoaded = true
+            } else {
+                Toast.makeText(this@MainActivity, "Gagal load", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        // Load raw clinking_glasses ke soundpool, jika selesai maka id-nya dimasukkan ke variable soundId
+        soundId = sp.load(this, R.raw.clinking_glasses, 1) // in 2nd param u have to pass your desire ringtone
+
+        btnSound.setOnClickListener {
+            if (spLoaded) {
+                sp.play(soundId, 1f, 1f, 0, 0, 1f)
+            }
         }
     }
 }
